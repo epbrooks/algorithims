96949a324d0cf6ff5105a7d642c0e21ebc92378f
epbrooks <123840563+epbrooks@users.noreply.github.com>
2023-03-17 14:49:04 -0700

    Added HW 2

---
+++ html/HW2.py
@@ -0,0 +1,5 @@
+# Split in Half
+# Given a string. Split it into two equal parts. Swap these parts and return the result.
+# If the string has odd characters, the first part should be one character greater than the second part.
+# Example: string = 'bbbbbcaaaaa'. Result = ‘aaaaabbbbbc’.
+
